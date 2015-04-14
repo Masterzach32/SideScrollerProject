73bd1051096fc2576413e36a222546e631d79b89
Masterzach32 <web32@masterzach32.net>
2015-04-14 17:08:50 -0400

    Quality of life improvement for log times

--- SideScroller/src/net/masterzach32/sidescroller/util/Utilities.java
+++ SideScroller/src/net/masterzach32/sidescroller/util/Utilities.java
@@ -10,7 +10,32 @@ public class Utilities {
 	 */
 	public static String getTime() {
 		LocalTime lt = LocalTime.now();
-		String s = new String(lt.getHour() + ":" + lt.getMinute() + ":" + lt.getSecond());
+		String hour = null;
+		String minute = null;
+		String second = null;
+		int i;
+		// get hour
+		i = lt.getHour();
+		if(i <= 9) {
+			hour = "0" + i;
+		} else if(i >= 10) {
+			hour = "" + i;
+		}
+		// get minute
+		i = lt.getMinute();
+		if(i <= 9) {
+			minute = "0" + i;
+		} else if(i >= 10) {
+			minute = "" + i;
+		}
+		// get second
+		i = lt.getSecond();
+		if(i <= 9) {
+			second = "0" + i;
+		} else if(i >= 10) {
+			second = "" + i;
+		}
+		String s = new String(hour + ":" + minute + ":" + second);
 		return s;
 	}
 }
