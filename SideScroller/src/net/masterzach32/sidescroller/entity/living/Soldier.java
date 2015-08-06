eb4a56954d5c4dc316f8e028cfe022f2ad0fcaf6
Masterzach32 <zachkozar@masterzach32.net>
2015-08-05 20:29:22 -0400

    Damage log no longer says that entities take type null damage from themselves when hit by conquering sands

--- SideScroller/src/net/masterzach32/sidescroller/entity/living/Soldier.java
+++ SideScroller/src/net/masterzach32/sidescroller/entity/living/Soldier.java
@@ -112,7 +112,7 @@ public class Soldier extends MapObject {
 				}
 			} else if(moving) {
 				if(e.intersects(this)) {
-					hit = e.hit(attack / 2, false, true, null, e);
+					hit = e.hit(attack / 2, false, true, "Conquering Sands", this);
 					e.addEffect(this, Effect.SLOW, 4 * level, 1);
 					addToHitList(e);
 				}
